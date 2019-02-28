a984c6c8c4c10c9503ff90af335810ba90ea2808
pritamkamble <7276279026.pk@gmail.com>
2019-03-01 01:20:52 +0530

    final app

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:hello/pages/landing_page.dart';
+import 'package:hello/pages/quiz_page.dart';
+import 'package:hello/pages/score_page.dart';
+
 void main() {
-  runApp(App());
-}
-
-class App extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<App> {
-  List<String> products = ['Food Tester'];
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList commit check'),
-        ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                onPressed: () {},
-                child: Text('Add Product'),
-              ),
-            ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/food.jpg'),
-                  Text("Food Paradise")
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  runApp(new MaterialApp(
+    home: new LandingPage(),
+  ));
 }
---
+++ lib/pages/quiz_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import 'package:hello/utils/question.dart';
+import 'package:hello/utils/quiz.dart';
+
+import 'package:hello/UI/answer_button.dart';
+import 'package:hello/UI/question_text.dart';
+import 'package:hello/UI/correct_wrong_overlay.dart';
+
+import 'package:hello/pages/score_page.dart';
+
+class QuizPage extends StatefulWidget {
+  @override
+  State createState() => new QuizPageState();
+}
+
+class QuizPageState extends State<QuizPage> {
+
+  Question currentQuestion;
+  Quiz quiz = new Quiz([
+    new Question("Elon Musk is human", false),
+    new Question("Pizza is healthy", false),
+    new Question("Flutter is awesome", true)
+  ]);
+  String questionText;
+  int questionNumber;
+  bool isCorrect;
+  bool overlayShouldBeVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    currentQuestion = quiz.nextQuestion;
+    questionText = currentQuestion.question;
+    questionNumber = quiz.questionNumber;
+  }
+
+  void handleAnswer(bool answer) {
+    isCorrect = (currentQuestion.answer == answer);
+    quiz.answer(isCorrect);
+    this.setState(() {
+      overlayShouldBeVisible = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        new Column( // This is our main page
+          children: <Widget>[
+            new AnswerButton(true, () => handleAnswer(true)), //true button
+            new QuestionText(questionText, questionNumber),
+            new AnswerButton(false, () => handleAnswer(false)), // false button
+          ],
+        ),
+        overlayShouldBeVisible == true ? new CorrectWrongOverlay(
+            isCorrect,
+                () {
+              if (quiz.length == questionNumber) {
+                Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(builder: (BuildContext context) => new ScorePage(quiz.score, quiz.length)), (Route route) => route == null);
+                return;
+              }
+              currentQuestion = quiz.nextQuestion;
+              this.setState(() {
+                overlayShouldBeVisible = false;
+                questionText = currentQuestion.question;
+                questionNumber = quiz.questionNumber;
+              });
+            }
+        ) : new Container()
+      ],
+    );
+  }
+}
